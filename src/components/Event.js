01f0b89ba7c86d32cadedc42f3d0aaba8e57ef22
Andrea <andreabarr87@gmail.com>
2020-03-25 12:55:23 -0500

    divs on divs on divs

--- src/components/Event.js
+++ src/components/Event.js
@@ -15,12 +15,34 @@ class Event extends React.Component {
 		console.log(id)
 	}
 	render() {
-		// console.log(this.props.eventObj.attributes.title)
 		return(
 				<li>
 					<div class="collapsible-header" onClick={(event) => this.handleClick(event)} >
-						<i class="material-icons">filter_drama</i>
-						<div id={this.props.eventObj.id}>{this.props.eventObj.attributes.title}</div>
+						<div class="col s1 pull-s11">
+							<div><span class="flow-text">{this.props.eventObj.attributes.start_date}</span></div>
+							<div><span class="flow-text">- {this.props.eventObj.attributes.end_date}</span></div>
+						</div>
+						<div class="col s1 pull-s10">
+							<img style={{maxHeight:100}} class="circle responsive-img" src={this.props.eventObj.attributes.img_url} />
+							</div>
+						<div class="col s4 pull-s8">
+							<div class="row">
+								<span class="flow-text">{this.props.eventObj.attributes.title}</span>
+							</div>
+							<div class="row">
+								<span>click for description</span>
+							</div>
+						</div>
+						<div class="col s1 pull-s6">
+							<div class="row">
+								<span class="flow-text">{this.props.eventObj.attributes.title}</span>
+							</div>
+							<div class="row">
+								<span>click for description</span>
+							</div>
+						</div>
+						{/* <i class="material-icons">filter_drama</i> */}
+						{/* <div id={this.props.eventObj.id}>{this.props.eventObj.attributes.title}</div> */}
 					</div>
 					<div class="collapsible-body">
 						<span>{this.props.eventObj.attributes.description}</span>
